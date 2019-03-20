96b3f7b16034c4c93bea8fec7997ad8443f05f90
Josh Mobley <joshua.mobley@consensys.net>
2019-03-20 15:56:00 -0400

    adding ghpages

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  pathPrefix: "/broz-chat",
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
